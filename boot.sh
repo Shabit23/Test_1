5e29b8b8c801601daafdd86e108510ee6e9dd673
M.S. Zaman Shabit <69386172+Shabit23@users.noreply.github.com>
2020-10-30 11:56:59 +0600

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -19,7 +19,7 @@ docker container run \
     --name=db \
     --env POSTGRES_DB=$DB_DATABASE \
     --env POSTGRES_PASSWORD_FILE=/run/secrets/dbPassword \
-    --volume -v ./data:/var/lib/postgresql/data
+    --volume -v ./data:/var/lib/postgresql/data \
     --network=notes-api-network \
     notes-db;
 printf "db container started --->\n"
